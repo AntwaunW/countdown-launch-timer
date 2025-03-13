07e6ff6fa4a5bf0dc2d28472c778701dcaf676ae
Antwaun Washington <antwaunwashington@gmail.com>
2025-03-12 22:17:19 -0500

    Function added

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const number = document.getElementById("number");
 
-function countDown () {
-    number.style.backgroundColor(orange);
-}
+// Set the date we're counting down to
+const targetDate = new Date("2025-03-15T12:00:00").getTime();
 
-countDown();
+// Update the countdown every 1 second
+const interval = setInterval(function() {
+
+  // Get today's date and time
+  const now = new Date().getTime();
+
+  // Find the distance between now and the countdown date
+  const distance = targetDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the results
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+
+  // If the countdown is finished, write some text
+  if (distance < 0) {
+    clearInterval(interval);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
